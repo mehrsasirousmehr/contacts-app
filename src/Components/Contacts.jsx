a145f36ed55d81ec6e4b5998ccc7134b17b2d565
Mehrsa <mhrs.project22@gmail.com>
2024-02-07 23:24:02 +0330

    create addHandler event && alert

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styles from "./Contacts.module.css";
 
 function Contacts() {
+    const [alert, setAlert] = useState("");
+    const [contacts, setContacts] = useState([]);
     const [contact, setContact] = useState({
         name: "",
         lastName: "",
@@ -15,6 +17,22 @@ function Contacts() {
         setContact((contact) => ({ ...contact, [name]: value }));
     };
 
+    // add contacts
+    const addHandler = () => {
+        if (contact.name && contact.lastName && contact.email && contact.phone) {
+            setContacts((contacts) => [...contacts, contact]);
+            setContact({
+                name: "",
+                lastName: "",
+                email: "",
+                phone: "",
+            });
+            setAlert("");
+        } else {
+            setAlert("please fill all the inputs");
+        }
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.form}>
@@ -24,6 +42,7 @@ function Contacts() {
                 <input type="number" placeholder="Phone" name="phone" value={contact.phone} onChange={changeHandler} />
                 <button onClick={addHandler}>Add contact</button>
             </div>
+            <div className={styles.alert}>{alert ? <p>{alert}</p> : ""}</div>
         </div>
     );
 }
