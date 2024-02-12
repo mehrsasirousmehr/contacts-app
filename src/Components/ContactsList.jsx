c5aac9e510cdd329841766db7cc74dfcf52216a2
Mehrsa <mhrs.project22@gmail.com>
2024-02-12 19:15:01 +0330

    render contacts

--- src/Components/ContactsList.jsx
+++ src/Components/ContactsList.jsx
@@ -1,13 +1,27 @@
 function ContactsList({ contacts }) {
-    console.log(contacts);
     return (
         <div>
             <h3>Contacts List</h3>
-            <ul>
-                {contacts.map((contact) => (
-                    <li key={contact.id}>{contact.name}</li>
-                ))}
-            </ul>
+            {contacts.length ? (
+                <ul>
+                    {contacts.map((contact) => (
+                        <li key={contact.id}>
+                            <p>
+                                {contact.name} {contact.lastName}
+                            </p>
+                            <p>
+                                <span>📧</span> {contact.email}
+                            </p>
+                            <p>
+                                <span>📞</span> {contact.phone}
+                            </p>
+                            <button>🗑</button>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No contacts yet!</p>
+            )}
         </div>
     );
 }
